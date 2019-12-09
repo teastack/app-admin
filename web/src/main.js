31916f6908ef8d0f52038aa0a227444eb4ccd146
Jim.D <839329266@qq.com>
2019-12-09 09:34:03 +0800

    添加vuex依赖和添加路由拦截功能

--- web/src/main.js
+++ web/src/main.js
@@ -18,6 +18,29 @@ Vue.use(ViewUI)
 
 Vue.config.productionTip = false
 
+// 添加路由全局守卫
+router.beforeEach((to, from, next) => {
+  // 判断是app还是后台管理
+  const isAdmin = to.path.indexOf('admin') ? 1 : 0
+  // 1.获取令牌：token
+  var token = localStorage.getItem('mytoken')
+  // 如果token有值，说明曾经登陆地 ，说明拥有合法访问的令牌
+  if (token) {
+    next()
+  } else {
+    // 如果是访问默认可以访问的页，如登陆，那么也不需要有令牌
+    if (to.path === '/login' || to.path === '/home' || to.path === '/admin/login') { // 判断是不是访问登陆页面
+      next()
+    } else {
+      if (isAdmin) {
+        next({path: '/admin/login'})
+      } else {
+        next({path: '/login'})
+      }
+    }
+  }
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
