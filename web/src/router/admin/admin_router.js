08fb860a205c9062706e0a7d12a8b727c99889c6
Jim.D <839329266@qq.com>
2019-12-09 09:35:36 +0800

    添加后台管理登录模块

--- web/src/router/admin/admin_router.js
+++ web/src/router/admin/admin_router.js
@@ -3,5 +3,10 @@ export default [
     path: 'home',
     name: 'admin-home',
     component: resolve => require(['@/views/admin/Home'], resolve)
+  },
+  {
+    path: 'login',
+    name: 'admin-login',
+    component: resolve => require(['@/views/admin/Login'], resolve)
   }
 ]
