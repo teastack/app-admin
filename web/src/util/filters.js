a1e94f014ff3b59fac24bd25352bf5cff5b6a919
Jim.D <839329266@qq.com>
2019-12-27 16:21:37 +0800

    添加时间过滤

--- web/src/util/filters.js
+++ web/src/util/filters.js
@@ -1,6 +1,5 @@
 // 年
 export function year (val, type) {
-  val = '2019-12-23 15:01:55'
   let date = new Date(val)
   let str = ''
   str = date.getFullYear()
@@ -60,4 +59,42 @@ export function seconds (val) {
     str += '0'
   }
   return str
+}
+
+export function timeago (dateTimeStampVal) {
+  let dateTimeStamp = new Date(dateTimeStampVal).getTime() // dateTimeStamp是一个时间毫秒，注意时间戳是秒的形式，在这个毫秒的基础上除以1000，就是十位数的时间戳。13位数的都是时间毫秒。
+  let minute = 1000 * 60 // 把分，时，天，周，半个月，一个月用毫秒表示
+  let hour = minute * 60
+  let day = hour * 24
+  let week = day * 7
+  // let halfamonth = day * 15
+  let month = day * 30
+  let now = new Date().getTime() // 获取当前时间毫秒
+  let diffValue = now - dateTimeStamp // 时间差
+
+  if (diffValue < 0) {
+    return
+  }
+  let minC = diffValue / minute // 计算时间差的分，时，天，周，月
+  let hourC = diffValue / hour
+  let dayC = diffValue / day
+  let weekC = diffValue / week
+  let monthC = diffValue / month
+  let result = ''
+  if (monthC >= 1 && monthC <= 3) {
+    result = ' ' + parseInt(monthC) + '月前 '
+  } else if (weekC >= 1 && weekC <= 3) {
+    result = ' ' + parseInt(weekC) + '周前 '
+  } else if (dayC >= 1 && dayC <= 6) {
+    result = ' ' + parseInt(dayC) + '天前 '
+  } else if (hourC >= 1 && hourC <= 23) {
+    result = ' ' + parseInt(hourC) + '小时前 '
+  } else if (minC >= 1 && minC <= 59) {
+    result = '  ' + parseInt(minC) + '分钟前 '
+  } else if (diffValue >= 0 && diffValue <= minute) {
+    result = '刚刚 '
+  } else {
+    result = dateTimeStampVal
+  }
+  return result
 }
