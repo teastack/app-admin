4cbaf0b9202a4bee89a840c6af7139c92116fcc2
Jim.D <839329266@qq.com>
2020-01-13 17:04:15 +0800

    角色

---
+++ web/src/views/admin/privilege_management/Role_detailed.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="role_list">
+    <div>
+        <!-- <Row class="expand-row">
+            <Col span="8">
+                <span class="expand-key">Job: </span>
+                <span class="expand-value">{{ row.job }}</span>
+            </Col>
+            <Col span="8">
+                <span class="expand-key">Interest: </span>
+                <span class="expand-value">{{ row.interest }}</span>
+            </Col>
+            <Col span="8">
+                <span class="expand-key">Birthday: </span>
+                <span class="expand-value">{{ row.birthday }}</span>
+            </Col>
+        </Row>
+        <Row>
+            <Col span="8">
+                <span class="expand-key">Favorite book: </span>
+                <span class="expand-value">《{{ row.book }}》</span>
+            </Col>
+            <Col span="8">
+                <span class="expand-key">Favorite movie: </span>
+                <span class="expand-value">{{ row.movie }}</span>
+            </Col>
+            <Col span="8">
+                <span class="expand-key">Favorite music: </span>
+                <span class="expand-value">{{ row.music }}</span>
+            </Col>
+        </Row> -->
+        <div class="tag">
+            <div class="tag1">
+            <Tag type="dot" closable color="primary" @on-close="handleClose">标签一</Tag>
+            </div>
+            <div class="tag2">
+            <Tag type="dot" closable color="success">标签二</Tag>
+            </div>
+            <div class="tag3">
+            <Tag type="dot" closable color="warning">标签三</Tag>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'role-detailed',
+  data () {
+    return {
+    }
+  },
+  props: {
+    row: Object
+  },
+  created () {
+  },
+  mounted () {
+  },
+  methods: {
+    handleClose () {
+      console.log('关闭')
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+    .expand-row{
+        margin-bottom: 16px;
+    }
+    .tag {
+        display: flex
+    }
+</style>
--- web/src/views/admin/privilege_management/Role_list.vue
+++ web/src/views/admin/privilege_management/Role_list.vue
@@ -1,23 +1,136 @@
 <template>
   <div class="role_list">
+    <Table :columns="columns10" border :data="data9"></Table>
   </div>
 </template>
 
 <script>
 
 import Api from '@/api/admin/admin_api'
+import expandRow from './Role_detailed'
 
 export default {
   name: 'role-list',
   data () {
     return {
-      roleList: []
+      roleList: [],
+      columns10: [
+        {
+          type: 'expand',
+          width: 50,
+          render: (h, params) => {
+            return h(expandRow, {
+              props: {
+                row: params.row
+              }
+            })
+          }
+        },
+        {
+          title: '名称',
+          key: 'name'
+        },
+        {
+          title: '描述',
+          key: 'desc'
+        },
+        {
+          title: '操作',
+          key: 'operation',
+          render: (h, params) => {
+            return h('div', [
+              h('Button', {
+                props: {
+                  type: 'primary',
+                  size: 'small'
+                },
+                style: {
+                  marginRight: '5px'
+                },
+                on: {
+                  click: () => {
+                    this.show(params.index)
+                  }
+                }
+              }, '查看'),
+              h('Button', {
+                props: {
+                  type: 'error',
+                  size: 'small'
+                },
+                on: {
+                  click: () => {
+                    this.remove(params.index)
+                  }
+                }
+              }, '删除')
+            ])
+          }
+        }
+      ],
+      data9: [
+        {
+          name: 'John Brown',
+          age: 18,
+          address: 'New York No. 1 Lake Park',
+          job: 'Data engineer',
+          interest: 'badminton',
+          birthday: '1991-05-14',
+          book: 'Steve Jobs',
+          movie: 'The Prestige',
+          music: 'I Cry'
+        },
+        {
+          name: 'Jim Green',
+          age: 25,
+          address: 'London No. 1 Lake Park',
+          job: 'Data Scientist',
+          interest: 'volleyball',
+          birthday: '1989-03-18',
+          book: 'My Struggle',
+          movie: 'Roman Holiday',
+          music: 'My Heart Will Go On'
+        },
+        {
+          name: 'Joe Black',
+          age: 30,
+          address: 'Sydney No. 1 Lake Park',
+          job: 'Data Product Manager',
+          interest: 'tennis',
+          birthday: '1992-01-31',
+          book: 'Win',
+          movie: 'Jobs',
+          music: 'Don’t Cry'
+        },
+        {
+          name: 'Jon Snow',
+          age: 26,
+          address: 'Ottawa No. 2 Lake Park',
+          job: 'Data Analyst',
+          interest: 'snooker',
+          birthday: '1988-7-25',
+          book: 'A Dream in Red Mansions',
+          movie: 'A Chinese Ghost Story',
+          music: 'actor'
+        }
+      ]
+    }
+  },
+  methods: {
+    show (index) {
+      this.$Modal.info({
+        title: 'User Info',
+        content: `Name：${this.data9[index].name}<br>Age：${this.data9[index].age}<br>Address：${this.data9[index].address}`
+      })
+    },
+    remove (index) {
+      this.data9.splice(index, 1)
     }
   },
   created () {
     Api.getRoleList().then(res => {
       if (res.code === 200) {
-        this.roleList = res.data
+        this.data9 = res.data
       }
     })
   },
@@ -28,7 +141,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
-    .expand-row{
-        margin-bottom: 16px;
-    }
+.role_list {
+  padding: 10px;
+  .expand-row{
+      margin-bottom: 16px;
+  }
+}
 </style>
